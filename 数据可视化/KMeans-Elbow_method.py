12e4425c6cc79b8167de0ee2266323798034c38c
QUELUCIFER <43593120+QUELUCIFER@users.noreply.github.com>
2019-07-12 22:07:08 +0800

    Create KMeans-Elbow_method.py

---
+++ 数据可视化/KMeans-Elbow_method.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('Wholesale customers data.csv')
+data.head()
+
+
+categorical_features = ['Channel', 'Region']
+continuous_features = ['Fresh', 'Milk', 'Grocery', 'Frozen', 'Detergents_Paper', 'Delicassen']
+
+data[continuous_features].describe()
+
+
+
+for col in categorical_features:
+    dummies = pd.get_dummies(data[col], prefix=col)
+    data = pd.concat([data, dummies], axis=1)
+    data.drop(col, axis=1, inplace=True)
+data.head()
+
+mms = MinMaxScaler()
+mms.fit(data)
+data_transformed = mms.transform(data)
+
+
+Sum_of_squared_distances = []
+K = range(1,15)
+for k in K:
+    km = KMeans(n_clusters=k)
+    km = km.fit(data_transformed)
+    Sum_of_squared_distances.append(km.inertia_)
+
+plt.plot(K, Sum_of_squared_distances, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
+
